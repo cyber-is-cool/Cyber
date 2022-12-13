267f53074da008606f0fc874ef979124c83660b5
cyber-is-cool <120389483+cyber-is-cool@users.noreply.github.com>
2022-12-12 19:48:30 -0600

    update
    more code

--- Bigboi.sh
+++ Bigboi.sh
@@ -8,6 +8,12 @@ function update_remove_packets {
 	apt remove nginx
 	apt remove snmp
 	apt remove xinetd
+	local PACKAGE_REMOVE
+	PACKAGE_REMOVE="apport* autofs avahi* beep git pastebinit popularity-contest rsh* rsync talk* telnet* tftp* whoopsie xinetd yp-tools ypbind"
+	
+	for deb_remove in $PACKAGE_REMOVE; do
+		apt-get remove "$deb_remove" -y
+	done
 }
 
 function FTP {  
@@ -224,21 +230,6 @@ function fireball {
 		clear
 	fi	
 }
-function virus {
-	#Lynis
-	apt-get install git
-	clear
-	echo "Lynis"
-	sleep 1
-	clear
-	
-	git clone https://github.com/CISOfy/lynis
-	cd lynis || return
-	sleep 2
-	clear
-	./lynis audit system
-	read -p "enter" i
-}
 function bad_pro {
 	echo "removing DCCP SCTP RDS TIPC protocols"
 	sleep 2
@@ -260,15 +251,140 @@ function bad_pro {
 	install hfsplus /bin/true
 	install udf /bin/true
 	install vfat /bin/true
+	
+	sleep 2
+	clear 
+	echo "who knows if that worked check /dev/null"
+	sleep 2
+}
+function system {
+	sed -i 's/^#DumpCore=.*/DumpCore=no/' "/etc/systemd/system.conf"
+	sed -i 's/^#CrashShell=.*/CrashShell=no/' "/etc/systemd/system.conf"
+	sed -i 's/^#DefaultLimitCORE=.*/DefaultLimitCORE=0/' "/etc/systemd/system.conf"
+	sed -i 's/^#DefaultLimitNOFILE=.*/DefaultLimitNOFILE=1024/' "/etc/systemd/system.conf"
+	sed -i 's/^#DefaultLimitNPROC=.*/DefaultLimitNPROC=1024/' "/etc/systemd/system.conf"
+	sleep 1
+	sed -i 's/^#DefaultLimitCORE=.*/DefaultLimitCORE=0/' "$USERCONF"
+	sed -i 's/^#DefaultLimitNOFILE=.*/DefaultLimitNOFILE=1024/' "$USERCONF"
+	sed -i 's/^#DefaultLimitNPROC=.*/DefaultLimitNPROC=1024/' "$USERCONF"
+	sleep 1
+	clear
+	echo "system hard"
+	clear
+	echo "hosts things"
+	sleep 2	
+	echo "sshd : ALL : ALLOW" >> /etc/hosts.allow
+	echo "ALL: LOCAL, 127.0.0.1" >> /etc/hosts.allow
+	echo "ALL: ALL" > /etc/hosts.deny
+	chmod 644 /etc/hosts.allow
+	chmod 644 /etc/hosts.deny
+	
+}
+function virus {
+	#Lynis
+	apt-get install git
+	clear
+	echo "Lynis"
+	sleep 1
+	clear
+	
+	git clone https://github.com/CISOfy/lynis
+	cd lynis || return
+	sleep 2
+	clear
+	#./lynis audit system
+	read -p "enter" X
+	clear
+}
+function login_security {
+	echo "login stuff"
+
+	sed -i 's/^.*LOG_OK_LOGINS.*/LOG_OK_LOGINS yes/' "/etc/login.defs"
+	sed -i 's/^UMASK.*/UMASK 077/' "/etc/login.defs"
+	sed -i 's/^PASS_MIN_DAYS.*/PASS_MIN_DAYS 7/' "/etc/login.defs"
+	sed -i 's/^PASS_MAX_DAYS.*/PASS_MAX_DAYS 60/' "/etc/login.defs"
+	sed -i 's/DEFAULT_HOME.*/DEFAULT_HOME no/' "/etc/login.defs"
+	sed -i 's/ENCRYPT_METHOD.*/ENCRYPT_METHOD SHA512/' "/etc/login.defs"
+	sed -i 's/USERGROUPS_ENAB.*/USERGROUPS_ENAB no/' "/etc/login.defs"
+	sed -i 's/^#.*SHA_CRYPT_MIN_ROUNDS .*/SHA_CRYPT_MIN_ROUNDS 10000/' "/etc/login.defs"
+	sed -i 's/^#.*SHA_CRYPT_MAX_ROUNDS .*/SHA_CRYPT_MAX_ROUNDS 65536/' "/etc/login.defs"
+	echo "done"
+	sleep 2
+	clear
+}
+function sysctl_hard {
+	 sysctl -w dev.tty.ldisc_autoload=0
+	 sysctl -w fs.protected_fifos=2
+	 sysctl -w fs.protected_hardlinks=1
+	 sysctl -w fs.protected_symlinks=1
+	 sysctl -w fs.suid_dumpable=0
+	 sysctl -w kernel.core_uses_pid=1
+	 sysctl -w kernel.dmesg_restrict=1
+	 sysctl -w kernel.kptr_restrict=2
+	 sysctl -w kernel.panic=60
+	 sysctl -w kernel.panic_on_oops=60
+	 sysctl -w kernel.perf_event_paranoid=3
+	 sysctl -w kernel.randomize_va_space=2
+	 sysctl -w kernel.sysrq=0
+	 sysctl -w kernel.unprivileged_bpf_disabled=1
+	 sysctl -w kernel.yama.ptrace_scope=2
+	 sysctl -w net.core.bpf_jit_harden=2
+	 sysctl -w net.ipv4.conf.all.accept_redirects=0
+	 sysctl -w net.ipv4.conf.all.accept_source_route=0
+	 sysctl -w net.ipv4.conf.all.log_martians=1
+	 sysctl -w net.ipv4.conf.all.rp_filter=1
+	 sysctl -w net.ipv4.conf.all.secure_redirects=0
+	 sysctl -w net.ipv4.conf.all.send_redirects=0
+	 sysctl -w net.ipv4.conf.all.shared_media=0
+	 sysctl -w net.ipv4.conf.default.accept_redirects=0
+	 sysctl -w net.ipv4.conf.default.accept_source_route=0
+	 sysctl -w net.ipv4.conf.default.log_martians=1
+	 sysctl -w net.ipv4.conf.default.rp_filter=1
+	 sysctl -w net.ipv4.conf.default.secure_redirects=0
+	 sysctl -w net.ipv4.conf.default.send_redirects=0
+	 sysctl -w net.ipv4.conf.default.shared_media=0
+	 sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=1
+	 sysctl -w net.ipv4.icmp_ignore_bogus_error_responses=1
+	 sysctl -w net.ipv4.ip_forward=0
+	 sysctl -w net.ipv4.tcp_challenge_ack_limit=2147483647
+	 sysctl -w net.ipv4.tcp_invalid_ratelimit=500
+	 sysctl -w net.ipv4.tcp_max_syn_backlog=20480
+	 sysctl -w net.ipv4.tcp_rfc1337=1
+	 sysctl -w net.ipv4.tcp_syn_retries=5
+	 sysctl -w net.ipv4.tcp_synack_retries=2
+	 sysctl -w net.ipv4.tcp_syncookies=1
+	 sysctl -w net.ipv6.conf.all.accept_ra=0
+	 sysctl -w net.ipv6.conf.all.accept_redirects=0
+	 sysctl -w net.ipv6.conf.all.accept_source_route=0
+	 sysctl -w net.ipv6.conf.all.forwarding=0
+	 sysctl -w net.ipv6.conf.all.use_tempaddr=2
+	 sysctl -w net.ipv6.conf.default.accept_ra=0
+	 sysctl -w net.ipv6.conf.default.accept_ra_defrtr=0
+	 sysctl -w net.ipv6.conf.default.accept_ra_pinfo=0
+	 sysctl -w net.ipv6.conf.default.accept_ra_rtr_pref=0
+	 sysctl -w net.ipv6.conf.default.accept_redirects=0
+	 sysctl -w net.ipv6.conf.default.accept_source_route=0
+	 sysctl -w net.ipv6.conf.default.autoconf=0
+	 sysctl -w net.ipv6.conf.default.dad_transmits=0
+	 sysctl -w net.ipv6.conf.default.max_addresses=1
+	 sysctl -w net.ipv6.conf.default.router_solicitations=0
+	 sysctl -w net.ipv6.conf.default.use_tempaddr=2
+	 sysctl -w net.ipv6.conf.eth0.accept_ra_rtr_pref=0
+	 sysctl -w net.filter.nf_conntrack_max=2000000
+	 sysctl -w net.filter.nf_conntrack_tcp_loose=0
+	 sysctl -w kernel.panic=10
+	 sysctl -w kernel.modules_disabled=1  
+	 #chmod 0600 "$SYSCTL"
+	 systemctl restart systemd-sysctl
+	 sleep 2
+	 clear
+	 echo "sys stuff done"
+	 sleep 2
 }
 
 
 
-
-
-
-
-#update_remove_packets
+update_remove_packets
 #FTP
 #Samba
 #Tft
@@ -276,24 +392,11 @@ function bad_pro {
 #remove_other
 #Mail_time
 #fireball
+#bad_pro
+#system
 #virus
-bad_pro
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#login_security
+#sysctl_hard
 
 
 
