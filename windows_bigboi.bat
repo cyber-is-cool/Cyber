afb48dc35da478b358dabedf79336991c8fbf2f3
cyber-is-cool <120389483+cyber-is-cool@users.noreply.github.com>
2023-01-19 11:28:48 -0600

    f

--- windows_bigboi.bat
+++ windows_bigboi.bat
@@ -157,27 +157,18 @@ netsh advfirewall firewall add rule name="Block MSN
 
 ECHO Networking stuff
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\NetBT\Parameters" /v "TransportBindName" /t REG_SZ /d " " /f
-
-rem Disable File and Printer Sharing for Microsoft Networks
+ECHO Disable File and Printer Sharing for Microsoft Networks
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" /v "AutoShareServer" /t REG_DWORD /d 0 /f
-
-rem Disable QoS
+ECHO Disable QoS
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\QoS" /v "Start" /t REG_DWORD /d 4 /f
-
-rem Disable Microsoft Network Adapter Multiplexor Protocol
+ECHO Microsoft Network Adapter Multiplexor Protocol
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Netman" /v "Start" /t REG_DWORD /d 4 /f
-
-rem Disable Microsoft LLDP Protocol Driver
+ECHO LLDP Protocol Driver
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\lltdio" /v "Start" /t REG_DWORD /d 4 /f
-
-rem Disable Link Layer Topology Discovery Mapper IO Driver
+ECHO Start Services
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\lltdsvc" /v "Start" /t REG_DWORD /d 4 /f
-
-rem Disable Link Layer Topology Discovery Responder
+ECHO TCP disble IPV6
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip6" /v "DisabledComponents" /t REG_DWORD /d 0xffffffff /f
-
-rem Disable Internet Protocol version 6
+ECHO diabling IPv6
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\TCPIP6" /v "Start" /t REG_DWORD /d 4 /f
-
-rem 'WINS' tab, select 'Disable NETBIOS over TCP/IP'
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\NetBT\Parameters" /v "TransportBindName" /t REG_SZ /d " " /f
